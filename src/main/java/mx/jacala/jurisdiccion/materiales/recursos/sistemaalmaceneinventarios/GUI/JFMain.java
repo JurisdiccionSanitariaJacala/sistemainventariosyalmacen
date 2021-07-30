eb842de1554642c98a471eab7ddf53764f21bd84
Christian Yamil Castillo <cyccov@gmail.com>
2021-07-30 14:09:33 -0500

    Second commit
    Added a few classes

--- src/main/java/mx/jacala/jurisdiccion/materiales/recursos/sistemaalmaceneinventarios/GUI/JFMain.java
+++ src/main/java/mx/jacala/jurisdiccion/materiales/recursos/sistemaalmaceneinventarios/GUI/JFMain.java
@@ -32,11 +32,18 @@ public class JFMain extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenu8 = new javax.swing.JMenu();
+        jMenu9 = new javax.swing.JMenu();
+        jMenu10 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
 
         javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
         jFrame1.getContentPane().setLayout(jFrame1Layout);
@@ -62,9 +69,9 @@ public class JFMain extends javax.swing.JFrame {
             .addGap(0, 431, Short.MAX_VALUE)
         );
 
-        jMenu1.setText("File");
+        jMenu1.setText("Sistema");
 
-        jMenuItem1.setText("jMenuItem1");
+        jMenuItem1.setText("Salir");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem1ActionPerformed(evt);
@@ -72,26 +79,45 @@ public class JFMain extends javax.swing.JFrame {
         });
         jMenu1.add(jMenuItem1);
 
-        jMenuItem2.setText("jMenuItem2");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem2);
-
-        jMenuItem3.setText("jMenuItem3");
-        jMenu1.add(jMenuItem3);
-
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Edit");
+        jMenu2.setText("Catalogos");
+
+        jMenu6.setText("Insumos");
+        jMenu2.add(jMenu6);
+
+        jMenu7.setText("Destinos");
+
+        jMenuItem6.setText("Alta");
+        jMenu7.add(jMenuItem6);
+
+        jMenuItem7.setText("Consulta");
+        jMenu7.add(jMenuItem7);
+
+        jMenu2.add(jMenu7);
+
+        jMenu8.setText("jMenu8");
+        jMenu2.add(jMenu8);
+
+        jMenu9.setText("jMenu9");
+        jMenu2.add(jMenu9);
+
+        jMenu10.setText("jMenu10");
+        jMenu2.add(jMenu10);
+
         jMenuBar1.add(jMenu2);
 
         jMenu3.setText("jMenu3");
         jMenuBar1.add(jMenu3);
 
-        jMenu4.setText("jMenu4");
+        jMenu4.setText("Ayuda");
+
+        jMenuItem4.setText("Acerca de");
+        jMenu4.add(jMenuItem4);
+
+        jMenuItem5.setText("Ver manual");
+        jMenu4.add(jMenuItem5);
+
         jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
@@ -114,13 +140,6 @@ public class JFMain extends javax.swing.JFrame {
         // TODO add your handling code here:
         createFrame();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
-
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
-        JFrmAltaClave frmAltaClave = new JFrmAltaClave();
-        JDPMain.add(frmAltaClave);
-        frmAltaClave.setVisible(true);
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -164,12 +183,19 @@ public class JFMain extends javax.swing.JFrame {
     private javax.swing.JDesktopPane JDPMain;
     private javax.swing.JFrame jFrame1;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu10;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
+    private javax.swing.JMenu jMenu9;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/main/java/mx/jacala/jurisdiccion/materiales/recursos/sistemaalmaceneinventarios/classes/JDateUtils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.jacala.jurisdiccion.materiales.recursos.sistemaalmaceneinventarios.classes;
+
+/**
+ *
+ * @author INVENTARIOS
+ */
+public class JDateUtils {
+    
+}
